1d65232bca9ae725d244bd01ad248706904fa83e
Utsav-Khamar <utsavkhamar2@gmail.com>
2026-05-20 02:34:26 -0400

    Add function for computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello world!");
+
+function getComputerChoice() {
+    // choose a random interger among 0,1 and 2
+    let choice = Math.floor(Math.random() * 3)
+    if (choice === 0){
+        return "Rock";
+    }
+    else if (choice === 1){
+        return "Paper";
+    }
+    else {
+        return "Scissor";
+    }
+}
+
+console.log(getComputerChoice());
