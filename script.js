c120115ff9dd4e6db0e24e9cae18a75998d7ce49
Utsav-Khamar <utsavkhamar2@gmail.com>
2026-05-21 04:45:33 -0400

    Declare & Initialize round variable, Call playRound using iteration, Add comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Computer Choice
 function getComputerChoice() {
     // choose a random interger among 0,1 and 2
     let choice = Math.floor(Math.random() * 3)
@@ -12,6 +13,7 @@ function getComputerChoice() {
     }
 }
 
+//Human Choice
 function getHumanChoice() {
     //prompt user for input
     let choice = prompt("Rock, Paper or Scissor?");
@@ -24,58 +26,62 @@ function getHumanChoice() {
     return choice;
 }
 
-
-
+//Game Logic
 function playGame(){
 
     //Declare variables for human and computer score
     let humanScore = 0;
     let computerScore = 0;
+    let round = 5;
 
+    //Function to play a single round of game
     function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice){
-        console.log("Tie");
-        humanScore = humanScore + 1;
-        computerScore = computerScore + 1;
-    }
-    else if (humanChoice === "Rock"){
-        if (computerChoice === "Paper"){
-            console.log(`You lose. ${computerChoice} beats ${humanChoice}`);
+        
+        if (humanChoice === computerChoice){
+            console.log("Tie");
+            humanScore = humanScore + 1;
             computerScore = computerScore + 1;
         }
+        else if (humanChoice === "Rock"){
+            if (computerChoice === "Paper"){
+                console.log(`You lose. ${computerChoice} beats ${humanChoice}`);
+                computerScore = computerScore + 1;
+            }
+            else {
+                console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+                humanScore = humanScore + 1;
+            }
+        }
+        else if (humanChoice === "Paper"){
+            if (computerChoice === "Scissor"){
+                console.log(`You lose. ${computerChoice} beats ${humanChoice}`)
+                computerScore = computerScore + 1;
+            }
+            else {
+                console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+                humanScore = humanScore + 1;
+            }
+        }
         else {
-            console.log(`You won! ${humanChoice} beats ${computerChoice}`);
-            humanScore = humanScore + 1;
+            if (computerChoice === "Rock"){
+                console.log(`You lose. ${computerChoice} beats ${humanChoice}`)
+                computerScore = computerScore + 1;
+            }
+            else {
+                console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+                humanScore = humanScore + 1;
+            }
         }
     }
-    else if (humanChoice === "Paper"){
-        if (computerChoice === "Scissor"){
-            console.log(`You lose. ${computerChoice} beats ${humanChoice}`)
-            computerScore = computerScore + 1;
-        }
-        else {
-            console.log(`You won! ${humanChoice} beats ${computerChoice}`);
-            humanScore = humanScore + 1;
-        }
-    }
-    else {
-        if (computerChoice === "Rock"){
-            console.log(`You lose. ${computerChoice} beats ${humanChoice}`)
-            computerScore = computerScore + 1;
-        }
-        else {
-            console.log(`You won! ${humanChoice} beats ${computerChoice}`);
-            humanScore = humanScore + 1;
-        }
+
+    //Call playRound using iteration
+    for(i=1; i<=round; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        console.log(humanChoice);
+        console.log(computerChoice);
+        
+        playRound(humanChoice, computerChoice);
     }
 }
-
-}
-
-
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
-
-console.log(humanChoice);
-console.log(computerChoice);
-playRound(humanChoice, computerChoice);
